e1141673ead78bb6a8b148ab6f32aa5e8b5b2057
zassi <zassi@DESKTOP-FMUH2TP>
2017-10-16 17:27:59 +0900

    first commit 1

---
+++ src/test002/Test002.java
@@ -0,0 +1,21 @@
+package test002;
+
+import java.util.Scanner;
+
+public class Test002 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Input Number > ");
+		int n = Integer.parseInt(scan.next());
+		
+		System.out.println("Number = "+n);
+		System.out.println("input RealNumber > ");
+		double m = Double.parseDouble(scan.next());
+		//scan‚Åæ‚è‚ñ‚¾‚à‚Ì‚Í•¶š—ñ‚È‚Ì‚ÅÀ”‚É•ÏŠ·
+		
+		System.out.println("RealNumber = "+m);
+	}
+
+}
